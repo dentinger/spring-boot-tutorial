13fc9efce1d6e38f15fa62e7488354d185f2c4a2
Shawn Sherwood <ss65560@ex.kroger.com>
2016-04-18 10:28:00 -0400

    Tweaked projection usage

--- src/main/java/com/example/repository/UserRepository.java
+++ src/main/java/com/example/repository/UserRepository.java
@@ -5,7 +5,7 @@ import com.example.domain.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(excerptProjection = User.WithReviews.class)
+//@RepositoryRestResource(excerptProjection = User.WithReviews.class)
 public interface UserRepository extends CrudRepository<User, Long> {
   User findByUserId(String userId) ;
 }
